0ea0d62dcd6534850bd5d445ef7d80b872551767
umaramasub <uramasubramanian@usfca.edu>
2017-10-10 19:01:34 -0700

    Method to parse xml files to get specific tags

--- src/main/scala/project/xmlparser.scala
+++ src/main/scala/project/xmlparser.scala
@@ -7,24 +7,30 @@ package project
 import java.io.File
 import java.io.FileInputStream
 import java.io.IOException
-import org.apache.tika.exception.TikaException
 import org.apache.tika.metadata.Metadata
 import org.apache.tika.parser.ParseContext
 import org.apache.tika.parser.xml.XMLParser
 import org.apache.tika.sax.BodyContentHandler
+import scala.xml.XML
 
 
-object xmlparser extends App{
-  //detecting the file type
-  val handler : BodyContentHandler = new BodyContentHandler()
-  val metadata : Metadata  = new Metadata()
-  val inputstream : FileInputStream  = new FileInputStream(new File("C:\\Users\\Uma\\Documents\\Umaramasub-final-project\\Docs\\ClinicalTrails\\NCT00193726.xml"))
-  val pcontext :  ParseContext  = new ParseContext()
+//
+//object xmlparser extends App{
+//  //detecting the file type
+//  val handler : BodyContentHandler = new BodyContentHandler()
+//  val metadata : Metadata  = new Metadata()
+//  val inputstream : FileInputStream  = new FileInputStream(new File("C:\\Users\\Uma\\Documents\\Umaramasub-final-project\\Docs\\ClinicalTrails\\NCT00193726.xml"))
+//  val pcontext :  ParseContext  = new ParseContext()
+//  //Xml parser
+//  val xmlparser : XMLParser = new XMLParser()
+//  val parsedFile= xmlparser.parse(inputstream, handler, metadata, pcontext)
+//  System.out.println("Contents of the document:" + handler.toString )
+//
+//}
 
-  //Xml parser
-  val xmlparser : XMLParser = new XMLParser()
-  xmlparser.parse(inputstream, handler, metadata, pcontext)
-
-  System.out.println("Contents of the document:" + handler.toString )
+object xmlparser extends App {
+  val xml = XML.loadFile("C:\\Users\\Uma\\Documents\\Umaramasub-final-project\\Docs\\ClinicalTrails\\NCT01019616.xml")
+  val temp = (xml \\ "clinical_study"\\"textblock").toString()
+  print(temp)
 
 }
